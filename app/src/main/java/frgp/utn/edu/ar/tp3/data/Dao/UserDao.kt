db00c898e7d831f975747cb20ed335fcc4bc11e0
lucas <lucasgruden0@protonmail.com>
2024-09-16 08:35:44 -0300

    se agrego a UserDao los metodos para obtener usuarios

--- app/src/main/java/frgp/utn/edu/ar/tp3/data/Dao/UserDao.kt
+++ app/src/main/java/frgp/utn/edu/ar/tp3/data/Dao/UserDao.kt
@@ -3,8 +3,10 @@ package frgp.utn.edu.ar.tp3.data.Dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import frgp.utn.edu.ar.tp3.data.Entity.User
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
@@ -14,4 +16,10 @@ interface UserDao {
     suspend fun update(user: User)
     @Delete
     suspend fun delete(user: User)
+
+    @Query("SELECT * from users WHERE id = :id")
+    fun getUser(id: Int): Flow<User>
+
+    @Query("SELECT * from users ORDER BY id ASC")
+    fun getAllUsers(): Flow<List<User>>
 }
