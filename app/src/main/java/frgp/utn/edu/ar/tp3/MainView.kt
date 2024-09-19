b87beb88c7aa993744ea1fa59d469a83abc6f18b
carlo-renosto <carlorenosto13@gmail.com>
2024-09-19 02:21:54 -0300

    Añadir componente MainView

---
+++ app/src/main/java/frgp/utn/edu/ar/tp3/MainView.kt
@@ -0,0 +1,86 @@
+package frgp.utn.edu.ar.tp3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import frgp.utn.edu.ar.tp3.ui.theme.TP3Theme
+
+class MainView : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP3Theme {
+                Scaffold(
+                    topBar = {
+                        TopBarMainView()
+                    },
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    MainViewPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarMainView() {
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = { Text(text = "Parking Control") },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Red,
+            titleContentColor = Color.White
+        ),
+        actions = {
+            IconButton(onClick = { expanded = true }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Opciones",
+                    tint = Color.White
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Cerrar sesion") },
+                    onClick = {
+                        expanded = false
+                    }
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun MainViewPage(modifier: Modifier = Modifier) {
+
+}
