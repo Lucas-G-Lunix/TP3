f11ddba1eb381eab8aeea3fc18e6d2869df76bb3
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-09-22 19:39:31 -0300

    Update MyAccount.kt

--- app/src/main/java/frgp/utn/edu/ar/tp3/activity/me/MyAccount.kt
+++ app/src/main/java/frgp/utn/edu/ar/tp3/activity/me/MyAccount.kt
@@ -1,6 +1,10 @@
 package frgp.utn.edu.ar.tp3.activity.me
 
+import android.app.Application
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
@@ -24,13 +29,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.room.Room
+import frgp.utn.edu.ar.tp3.activity.login.Login
 import frgp.utn.edu.ar.tp3.component.DrawerMenu
 import frgp.utn.edu.ar.tp3.component.DrawerMenuDefaultOptions
 import frgp.utn.edu.ar.tp3.data.entity.User
 import frgp.utn.edu.ar.tp3.data.logic.AuthManager
 import frgp.utn.edu.ar.tp3.ui.theme.TP3Theme
+import   frgp. utn. edu. ar. tp3.activity.signup.SignUpViewModel
+import frgp.utn.edu.ar.tp3.data.ParkingDatabase
+import frgp.utn.edu.ar.tp3.data.dao.UserDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MyAccount: ComponentActivity()  {
     private val authManager: AuthManager by viewModels()
@@ -42,57 +58,87 @@ class MyAccount: ComponentActivity()  {
                 ModalNavigationDrawer(
                     drawerContent = { DrawerMenu(authManager, DrawerMenuDefaultOptions, {}, "myAccount") }
                 ) {
-                Scaffold(
-                    topBar = { TopBar() },
-                    modifier = Modifier.fillMaxSize(),
-                ) {innerPadding ->
-                    MyAccountView(modifier = Modifier.padding(innerPadding), authManager)
+                    Scaffold(
+                        topBar = { TopBar() },
+                        modifier = Modifier.fillMaxSize(),
+                    ) {innerPadding ->
+                        MyAccountView(modifier = Modifier.padding(innerPadding), authManager)
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun TopBar() {
-    TopAppBar(
-        title = { Text(text = "Parking Control") },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Red,
-            titleContentColor = Color.White
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun TopBar() {
+        TopAppBar(
+            title = { Text(text = "Parking Control") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Red,
+                titleContentColor = Color.White
+            )
         )
-    )
-}
+    }
 
-@Composable
-fun MyAccountView(modifier: Modifier, authManager: AuthManager) {
-    val context = LocalContext.current
-    val us = remember {
-        mutableStateOf<User?>(null)
-    }
-    LaunchedEffect(key1 = Unit) {
-        authManager.getCurrentUser().collect { user ->
-            us.value = user
+
+    @Composable
+    fun MyAccountView(modifier: Modifier, authManager: AuthManager) {
+        val context = LocalContext.current
+        val us = remember {
+            mutableStateOf<User?>(null)
+        }
+        LaunchedEffect(key1 = Unit) {
+            authManager.getCurrentUser().collect { user ->
+                us.value = user
+            }
+        }
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(top = 32.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Row {
+                Text("Nombre: ${us.value?.name}", fontSize = 20.sp)
+            }
+            Row {
+                Text("Nombre de Usuario: ${us.value?.username}", fontSize = 20.sp)
+            }
+            Row {
+                Text("Mail: ${us.value?.mail}", fontSize = 20.sp)
+            }
+            Row {
+                Button(onClick = {
+                    us.value?.let {
+                        if(deleteUser(it)) {
+                            Toast.makeText(context, "Usuario ${us.value?.username} eliminado Correctamente!", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(context, Login::class.java).apply {
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            }
+                            ContextCompat.startActivity(context, intent, null)
+                        }
+                        else Toast.makeText(context, "Error al eliminar usuario: ${us.value?.username} ", Toast.LENGTH_SHORT).show()
+                    }
+                }) {
+                    Text("Borrar Usuario")
+                }
+            }
         }
     }
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .padding(top = 32.dp),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Row {
-            Text("Nombre: ${us.value?.name}", fontSize = 20.sp)
-        }
-        Row {
-            Text("Nombre de Usuario: ${us.value?.username}", fontSize = 20.sp)
-        }
-        Row {
-            Text("Mail: ${us.value?.mail}", fontSize = 20.sp)
+
+    fun deleteUser(user: User): Boolean{
+        val db: ParkingDatabase = Room.databaseBuilder(application.applicationContext, ParkingDatabase::class.java, "parking")
+            .allowMainThreadQueries().build()
+        val dao: UserDao = db.userDao()
+        try {
+            dao.delete(user)
+            return true
+        } catch (e: Exception) {
+            return false
         }
     }
-    }
+
 }
